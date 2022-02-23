09c9c6b41dacf3cfb1f41dbbb68ef90d9ead0290
Pacman Graphics <hakkatjakka66@gmail.com>
2022-02-23 18:55:01 +0100

    Changed #bedrock layers = 3
    Added more layers of bedrock attempt to stop leaking water.

--- Linux/src/make.sh
+++ Linux/src/make.sh
@@ -5,5 +5,3 @@ g++ -O3 -w -std=c++17 -m64 -c MCEditor/MCACoder.cpp
 g++ -O3 -w -std=c++17 -m64 -c MCEditor/MCEditor.cpp
 g++ -O3 -w -std=c++17 -m64 -c MCEditor/NBTCoder.cpp
 g++ -o ../caves cave.o functions.o globals.o MCACoder.o MCEditor.o NBTCoder.o -s -m64 -lm -lsfml-system -lz
-
-
