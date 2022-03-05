e051826b3656e405b8919c2edafe4f778aba5320
Pacman Graphics <hakkatjakka66@gmail.com>
2022-03-05 07:20:11 +0100

    ShaderTest

--- Linux_ShaderTest/src/make_caves.sh
+++ Linux_ShaderTest/src/make_caves.sh
@@ -1,8 +1,10 @@
 g++ -O3 -w -std=c++17 -m64 -c main.cpp
+g++ -O3 -w -std=c++17 -m64 -c shaders.cpp
 g++ -O3 -w -std=c++17 -m64 -c make_caves.cpp
 g++ -O3 -w -std=c++17 -m64 -c functions.cpp
 g++ -O3 -w -std=c++17 -m64 -c MCEditor/globals.cpp
 g++ -O3 -w -std=c++17 -m64 -c MCEditor/MCACoder.cpp
 g++ -O3 -w -std=c++17 -m64 -c MCEditor/MCEditor.cpp
 g++ -O3 -w -std=c++17 -m64 -c MCEditor/NBTCoder.cpp
-g++ -o ../make_caves main.o make_caves.o functions.o globals.o MCACoder.o MCEditor.o NBTCoder.o -s -m64 -lm -lsfml-system -lsfml-window -lsfml-graphics -lz
+g++ -o ../make_caves shaders.o main.o make_caves.o functions.o globals.o MCACoder.o MCEditor.o NBTCoder.o -s -m64 -lm -lsfml-system -lsfml-window -lsfml-graphics -lz -lX11 -lglut -lGLU -lGL
+
